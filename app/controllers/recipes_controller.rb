7ee5820abf93f0978f703cf5419303ca895340c9
isaka-lumato <75973193+isaka-lumato@users.noreply.github.com>
2022-04-21 19:44:28 +0300

    recipe view

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,13 @@
 class RecipesController < ApplicationController
-    def index
+  def index
     @recipe = Recipe.all
+  end
+
+  def destroy
+    puts "This is an attempt to make you stand out #{params}"
+    recipe_destroy = Recipe.find(params[:id])
+    recipe_destroy.destroy
+    flash[:notice] = "Food succesfully deleted"
+    redirect_to recipe_url
   end
 end
