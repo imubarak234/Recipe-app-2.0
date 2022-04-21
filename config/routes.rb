a9b3345d879b0ff8ed973137bfd72e9b0f09cc3f
imubarak234 <imubarak2424@gmail.com>
2022-04-21 15:29:23 +0100

    devise and food page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
+
+  get '/user', to: 'user#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
