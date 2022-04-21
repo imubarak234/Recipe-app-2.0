aec3468e588eaaa8bca7899e68956851c5d1815d
isaka-lumato <75973193+isaka-lumato@users.noreply.github.com>
2022-04-21 03:15:26 +0300

    Add nav-links and recipe views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  # get 'public_recipes/index'
+  # get 'general_shopping_lists/index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
