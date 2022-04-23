6212b9c4c5946547dbc24f8f2664973b563c107d
imubarak234 <imubarak2424@gmail.com>
2022-04-23 16:05:25 +0100

    test suits

---
+++ spec/integration/food_index_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'Food Index', type: :feature do
+  before :each do
+    one = User.create(email: 'user1@gmail.com', password: 'password', name: 'User1')
+
+    one_food = Food.create(name:"Akara", measurement_unit: "g", price: 3.5, user: one)
+    two_food = Food.create(name:"ChinChin", measurement_unit: "g", price: 3.5, user: one)
+
+    visit '/users/sign_in'
+    fill_in 'Email', with: 'user1@gmail.com'
+    fill_in 'Password', with: 'password'
+    click_button 'Log in'
+  end
+  it 'Check content on the page' do
+
+    visit "/foods"
+
+    expect(page).to have_content("Akara")
+    expect(page).to have_content("ChinChin")
+    expect(page).to have_content("g")
+    expect(page).to have_content(3.5)
+  end
+end
---
+++ spec/model/recipe_food_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe RecipeFood, type: :model do
+
+  subject { RecipeFood.new(quantity: 2.0, recipe_id: 1, food_id: 1) }
+
+  before { subject.save }
+
+  it 'Quantity should be greater than or equals to 0' do
+    subject.quantity = -1
+    expect(subject).to_not be_valid
+  end
+
+end
